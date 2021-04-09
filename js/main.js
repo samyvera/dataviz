360145a61efb692719ccc94524828d34f273fbf3
samy <samy_leni@hotmail.fr>
2021-04-09 13:23:57 +0200

    test

--- js/main.js
+++ js/main.js
@@ -4,49 +4,64 @@ let posX, posY;
 let cOffset = 0;
 
 function setup() {
-	createCanvas(innerWidth, innerHeight);
-	posX = innerWidth / 2;
-	posY = innerHeight / 2;
+  createCanvas(innerWidth, innerHeight, WEBGL);
+  posX = innerWidth / 2;
+  posY = innerHeight / 2;
 }
 
 function windowResized() {
-	resizeCanvas(innerWidth, innerHeight);
-	posX = innerWidth / 2;
-	posY = innerHeight / 2;
+  resizeCanvas(innerWidth, innerHeight);
+  posX = innerWidth / 2;
+  posY = innerHeight / 2;
 }
 
+var theta = 0;
 let img;
+let img2;
 function preload() {
   img = loadImage('https://files.catbox.moe/nicp0s.png');
+  img2 = loadImage('https://files.catbox.moe/41x6d3.jpg');
 }
 
 function draw() {
-	const inc = height/100;
+  translate(-innerWidth / 2, -innerHeight / 2);
+  const inc = height/100;
     colorMode(HSB);
-
     for(let y = 0; y < height; y += inc) {
         let h = y / height * 360;
-        fill(abs(h-cOffset)%360, 100, 100, 50);
+        fill(abs(h-cOffset)%360, 100, 100);
         noStroke();
         rect(0, y-inc, width, y);
     }
 
     cOffset += 5;
 
-	for (let j = 1; j < 10; j++) {
-		fill('#fff');
-		stroke('#0000');
-		ellipse(posX + x * 1 / j, posY + y * 1 / j, 64 * 1 / j, 64 * 1 / j);
-		ellipse(posX - x * 1 / j, posY - y * 1 / j, 64 * 1 / j, 64 * 1 / j);
-		fill('#000');
-		stroke('#0000');
-		ellipse(posX + x * 1 / j, posY - y * 1 / j, 64 * 1 / j, 64 * 1 / j);
-		ellipse(posX - x * 1 / j, posY + y * 1 / j, 64 * 1 / j, 64 * 1 / j);
-	}
-	
-	image(img, innerWidth / 2 - 270, innerHeight / 2 - 270, 540, 540);
+  for (let j = 1; j < 10; j++) {
+    fill('#fff');
+    stroke('#0000');
+    ellipse(posX + x * 1 / j, posY + y * 1 / j, 64 * 1 / j, 64 * 1 / j);
+    ellipse(posX - x * 1 / j, posY - y * 1 / j, 64 * 1 / j, 64 * 1 / j);
+    fill('#000');
+    stroke('#0000');
+    ellipse(posX + x * 1 / j, posY - y * 1 / j, 64 * 1 / j, 64 * 1 / j);
+    ellipse(posX - x * 1 / j, posY + y * 1 / j, 64 * 1 / j, 64 * 1 / j);
+  }
+  image(img, innerWidth / 2 - 270, innerHeight / 2 - 270, 540, 540);
+  
+  translate(innerWidth / 2 + Math.cos(theta) * innerWidth / 2, innerHeight / 2 + Math.sin(theta) * innerHeight / 2);
+  rotateX(theta * mouseX * 0.001);
+  rotateZ(theta * mouseX * 0.001);
+  rotateY(theta * mouseX * 0.001);
+  noFill();
+  noStroke();
+  push();
+  texture(img2);
+  sphere(300);
+  pop();
+  
+  theta += 0.05;
 
-	x = Math.cos(i * 0.125) * innerWidth / 2;
-	y = Math.sin(i * 0.125) * innerHeight / 2;
-	i++;
+  x = Math.cos(i * 0.125) * innerWidth / 2;
+  y = Math.sin(i * 0.125) * innerHeight / 2;
+  i++;
 }
